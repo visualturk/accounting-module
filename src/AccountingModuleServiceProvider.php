a701cbf7df310a79f2569e3f2aaa2ce4e4f28337
Ryan Thompson <service@aiwebsystems.com>
2015-03-21 10:46:39 -0500

    K

---
+++ src/AccountingModuleServiceProvider.php
@@ -0,0 +1,16 @@
+<?php namespace Anomaly\AccountingModule;
+
+use Anomaly\Streams\Platform\Addon\AddonServiceProvider;
+
+/**
+ * Class AccountingModuleServiceProvider
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\AccountingModule
+ */
+class AccountingModuleServiceProvider extends AddonServiceProvider
+{
+
+}
