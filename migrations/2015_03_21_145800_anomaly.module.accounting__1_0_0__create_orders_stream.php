be38354779ff5fb422441fa20c5b87d8b5846b3c
Ryan Thompson <service@aiwebsystems.com>
2015-03-21 10:07:46 -0500

    Hmm

--- migrations/2015_03_21_145800_anomaly.module.accounting__1_0_0__create_orders_stream.php
+++ migrations/2015_03_21_145800_anomaly.module.accounting__1_0_0__create_orders_stream.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Anomaly\Streams\Platform\Database\Migration\Migration;
 
-class AnomalyModuleAccounting_1_0_0_CreateAccountingStream extends Migration {
+class AnomalyModuleAccounting_1_0_0_CreateOrdersStream extends Migration {
 
 	/**
 	 * Run the migrations.
